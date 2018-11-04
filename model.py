1833e7120edf398e6c5b7ace998daee0febbd749
jiangyitong1998 <43108094+jiangyitong1998@users.noreply.github.com>
2018-11-04 13:20:59 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -172,9 +172,12 @@ def compute_accuracy(v_xs, v_ys):
   # f1_score = (2 * (precision * recall)) / (precision + recall)
 
 
-def weight_variable(shape):
-	initial = tf.truncated_normal(shape,stddev=0.01)
-	return tf.Variable(initial)
+def weight_variable(name,shapen):
+	# initial = tf.truncated_normal(shape,stddev=0.01)
+	# return tf.Variable(initial)
+  initial = tf.get_variable(name,shape=shapen,
+           initializer=tf.contrib.layers.xavier_initializer())
+  return tf.Variable(initial)
 
 def bias_variable(shape):
 	initial = tf.constant(0.1, shape=shape)
@@ -210,26 +213,27 @@ x_image = tf.reshape(xs,[-1,72,398,1])
 
 
 ##conv1 layer##
-W_conv1 = weight_variable([5,5,1,32])
+W_conv1 = weight_variable("W_conv1",[5,5,1,32])
 b_conv1 = bias_variable([32])
-h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1) #output size
-h_pool1 = max_pool_2x2(h_conv1)
+h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1)+b_conv1) #output size 72*398*32
+h_pool1 = max_pool_2x2(h_conv1) #36*199*32
 
 # ##conv1 layer##
-# W_conv2 = weight_variable([5,5,32,64])
-# b_conv2 = bias_variable([64])
-# h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2) #output size
-# h_pool2 = max_pool_2x2(h_conv2) 
+W_conv2 = weight_variable("W_conv2",[5,5,32,64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2)+b_conv2) #output size 36*199*64
+h_conv2_reshape = tf.reshape(h_conv2,[-1,18,398,64])
+h_pool2 = max_pool_2x2(h_conv2_reshape)             #9*199*64
 
 ## fc1 layer ##
-W_fc1 = weight_variable([36*199*32,1024])
+W_fc1 = weight_variable("W_fc1",[9*199*64,1024])
 b_fc1 = bias_variable([1024])
-h_pool1_flat = tf.reshape(h_pool1,[-1,36*199*32])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
+h_pool2_flat = tf.reshape(h_pool2,[-1,9*199*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 ## fc2 layer ##
-W_fc2 = weight_variable([1024, 2])
+W_fc2 = weight_variable("W_fc2",[1024, 2])
 b_fc2 = bias_variable([2])
 prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
@@ -248,8 +252,8 @@ sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 #print('lll')
-for i in range(3000):
-    train_batch_xs,train_batch_ys = next_batch(100,train_smiles, train_lables)
+for i in range(6000):
+    train_batch_xs,train_batch_ys = next_batch(200,train_smiles, train_lables)
     #print(i)
     #print(train_batch_ys)
     
@@ -258,16 +262,18 @@ for i in range(3000):
     #print(train_batch_xs.shape,train_batch_ys.shape)
     #test_batch_xs,test_batch_ys = next_batch(test_smile, test_lable, batchSize) #print(batch_xs)
     #print(sess.run(b_conv1))
+    
     _, guess,cross = sess.run([train_step,prediction,cross_entropy], feed_dict={xs: train_batch_xs, ys: train_batch_ys, keep_prob: 0.5})
+    
     #sess.run(train_step, feed_dict={xs: train_batch_xs, ys: train_batch_ys, keep_prob: 0.5})
     #print(cross)
     #print(sess.run(h_pool1.shape))
     #print(test_smile[:265].shape, test_lable[:265].shape)
-    if i % 50 == 0:
+    if i % 100 == 0:
         print(compute_accuracy(
             test_smile[:265], test_lable[:265]))
         # print(compute_accuracy(
         #     train_batch_xs,train_batch_ys))
-saver = tf.train.Saver()
-saver.save(sess, './my_model', global_step = 1)
+# saver = tf.train.Saver()
+# saver.save(sess, './my_model', global_step = 1)
 
